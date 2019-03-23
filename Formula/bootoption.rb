fad8b644e9f19acb17c39f5ac168fe6df3acb89a
bootoption <48445527+bootoption@users.noreply.github.com>
2019-03-23 13:36:11 +0000

    0.4.0

--- Formula/bootoption.rb
+++ Formula/bootoption.rb
@@ -1,8 +1,8 @@
 class Bootoption < Formula
   desc "EFI boot menu manipulation for macOS"
   homepage "https://github.com/bootoption/bootoption/"
-  url "https://github.com/bootoption/bootoption/archive/0.3.7.tar.gz"
-  sha256 "3056ecef36029bd480c407f1f3ce07a629d3fc973d0cb1c2be627f43aaaf3afc"
+  url "https://github.com/bootoption/bootoption/archive/0.4.0.tar.gz"
+  sha256 "284f5411529bee580f5bfdf28e91db565b7fff8f78ded06061b5d321853fab3a"
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release", "-Xswiftc", "-static-stdlib"
     bin.install ".build/release/bootoption"
