a2f8eeaeac768b64d78cf4ba189f0ae795b56a5f
bootoption <48445527+bootoption@users.noreply.github.com>
2020-09-01 17:00:37 +0100

    0.6.0

--- Formula/bootoption.rb
+++ Formula/bootoption.rb
@@ -1,8 +1,8 @@
 class Bootoption < Formula
   desc "EFI boot menu manipulation for macOS"
   homepage "https://github.com/bootoption/bootoption/"
-  url "https://github.com/bootoption/bootoption/archive/0.5.0.tar.gz"
-  sha256 "24672162a86fa639682af317e656f321e0c386d6886b301f9797235c725cb6c0"
+  url "https://github.com/bootoption/bootoption/archive/0.6.0.tar.gz"
+  sha256 "c635749ca34756c142034bfd4ffd50f541a0f2198f3058c24fcef7372affbd92"
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/bootoption"
